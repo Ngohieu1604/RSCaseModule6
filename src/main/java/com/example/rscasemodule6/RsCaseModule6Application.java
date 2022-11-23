6d0cd8727d0024139bc5fb890787cd39ba48bad8
LENOVO <imbakito1604@gmail.com>
2022-11-23 14:08:48 +0700

    first commit

---
+++ src/main/java/com/example/rscasemodule6/RsCaseModule6Application.java
@@ -0,0 +1,13 @@
+package com.example.rscasemodule6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RsCaseModule6Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RsCaseModule6Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/rscasemodule6/RsCaseModule6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rscasemodule6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RsCaseModule6ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
